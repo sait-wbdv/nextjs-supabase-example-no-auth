37f5e38cee0c3926b4bc633fd24878dd65e001df
temielosait <temitope.elodimuor@sait.ca>
2024-12-04 13:51:40 -0700

    first pass

---
+++ app/countries/page.js
@@ -0,0 +1,26 @@
+'use client'
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+
+export default function Countries() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    getCountries();
+  }, []);
+
+  async function getCountries() {
+    const { data } = await supabase.from("countries").select();
+    setCountries(data);
+  }
+
+  return (
+    <ul>
+      {countries.map((country) => (
+        <li key={country.id} className="text-white">{country.country_name}</li>
+      ))}
+    </ul>
+  );
+}
